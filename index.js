8be098e83524e1820901c85fde17686ae616cdca
Mariam  Khamo <mariamw736@gmail.com>
2021-06-21 21:26:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function registerSelf() {
+  return fetch("http://guestbook.example.com/register", {
+    "method": "POST",
+    "body": {
+      "firstName": "BYRONIUS KARBITUS MARIS",
+      "registryMessage": "SALVETE AMICI!"
+    }
+  }).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    return json.message;
+  });
+}
+
+function errorSelf() {
+  return fetch("http://guestbook.example.com/register-error", {
+    "method": "POST",
+    "body": {
+      "firstName": "Byron the Poodle",
+      "registryMessage": "Bite!"
+    }
+  }).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    return json.message;
+  })
+}
